4f3fca0c4314d81428a74b8f64e508d2a6025cef
Anand-Ramnarain <125869920+Anand-Ramnarain@users.noreply.github.com>
2024-05-15 22:19:11 +0200

    changed the name of the folder

--- src/App.jsx
+++ src/App.jsx
@@ -14,11 +14,11 @@ const App = () => {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
-        <Films />
-        <Roadmap />
         <Collaboration />
-        <Services />
+        <Roadmap />
+        <Films />
         <Pricing />
+        <Services />
         <Footer />
       </div>
       <ButtonGradient />
