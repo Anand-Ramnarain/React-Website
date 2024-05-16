102c525624bc9d2078d50efd42fb032b105a1f4e
Anand-Ramnarain <125869920+Anand-Ramnarain@users.noreply.github.com>
2024-05-16 10:56:07 +0200

    Made a folder for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
-import ButtonGradient from "./assets/svg/ButtonGradient";
-import Header from "./components/Header";
-import Hero from "./components/Hero";
-import Films from "./components/Films";
-import Collaboration from "./components/Collaboration";
-import Services from "./components/Services";
-import Pricing from "./components/Pricing";
-import Roadmap from "./components/Roadmap";
-import Footer from "./components/Footer";
+import Pages from "./pages/Pages";
 
 const App = () => {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Header />
-        <Hero />
-        <Collaboration />
-        <Roadmap />
-        <Films />
-        <Pricing />
-        <Services />
-        <Footer />
+        <Pages />
       </div>
-      <ButtonGradient />
     </>
   );
 };
---
+++ src/pages/Pages.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Homepage from "./Homepage";
+
+const Pages = () => {
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
+};
+
+export default Pages;
