8405326fe71dba9aebba49a38a3ce834d66ad201
Anand-Ramnarain <125869920+Anand-Ramnarain@users.noreply.github.com>
2024-05-23 14:42:58 +0200

    change icon link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -39,7 +39,7 @@ const Header = () => {
       }`}
     >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
-        <Link to="/React-Website/" className="block w-[12rem] xl:mr-8">
+        <Link to="/" className="block w-[12rem] xl:mr-8">
           <img src={brainwave} width={190} height={40} alt="Website" />
         </Link>
 
