fd82ae6d6f9164b75127b282976d151b10c1f3f2
Anand-Ramnarain <125869920+Anand-Ramnarain@users.noreply.github.com>
2024-05-23 15:29:22 +0200

    added links to videos i used

--- src/App.js
+++ src/App.js
@@ -10,3 +10,10 @@ const App = () => {
 };
 
 export default App;
+
+//All Images i got from the internet
+// I used JavaScript Mastery video to learn this styling way
+// Links to videos are below:
+//https://www.youtube.com/watch?v=0fYi8SGA20k&list=PL6QREj8te1P7oAhnGpSuO1cwSi0xcLihD&index=1
+//https://www.youtube.com/watch?v=LMagNcngvcU&list=PL6QREj8te1P6CkO_4OIK1-nwG5OxCD5tR&index=3
+//https://www.youtube.com/watch?v=4oV65GVVits&list=PL6QREj8te1P6CkO_4OIK1-nwG5OxCD5tR&index=5&t=220s
