0bbac77add6b9387c63fc8f13cb4d1c3cfddeec3
Anand-Ramnarain <125869920+Anand-Ramnarain@users.noreply.github.com>
2024-05-23 12:29:36 +0200

    changed navbar linking to work

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./Homepage";
 import Layout from "../components/Layout";
 import TheoryWorks from "./TheoryWorks";
@@ -12,42 +11,44 @@ import { theorywork, design, about, art } from "../constants/information";
 
 const Pages = () => {
   return (
-    <Routes>
-      {/* Wrap the first three routes in the Layout component */}
-      <Route
-        path="/"
-        element={
-          <Layout>
-            <Homepage />
-          </Layout>
-        }
-      />
-      <Route
-        path="/theoryworks"
-        element={
-          <Layout>
-            <TheoryWorks />
-          </Layout>
-        }
-      />
-      <Route
-        path="/design"
-        element={
-          <Layout>
-            <DesignWorks />
-          </Layout>
-        }
-      />
-      {/* Define route for displaying essay content */}
-      <Route
-        path="/essay/:id"
-        element={<EssayContent theories={theorywork} />}
-      />
-      {/* Define route for displaying design content */}
-      <Route path="/design/:id" element={<DesignContent theories={design} />} />
-      <Route path="/about/:id" element={<AboutContent theories={about} />} />
-      <Route path="/art/:id" element={<ArtContent theories={art} />} />
-    </Routes>
+    <Router basename="/React-Website">
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <Homepage />
+            </Layout>
+          }
+        />
+        <Route
+          path="/theoryworks"
+          element={
+            <Layout>
+              <TheoryWorks />
+            </Layout>
+          }
+        />
+        <Route
+          path="/design"
+          element={
+            <Layout>
+              <DesignWorks />
+            </Layout>
+          }
+        />
+        <Route
+          path="/essay/:id"
+          element={<EssayContent theories={theorywork} />}
+        />
+        <Route
+          path="/design/:id"
+          element={<DesignContent theories={design} />}
+        />
+        <Route path="/about/:id" element={<AboutContent theories={about} />} />
+        <Route path="/art/:id" element={<ArtContent theories={art} />} />
+      </Routes>
+    </Router>
   );
 };
 
