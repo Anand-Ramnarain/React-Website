8cc7dc1dc9d182f80cdb834a15f74654efc7a053
Anand-Ramnarain <125869920+Anand-Ramnarain@users.noreply.github.com>
2024-05-23 12:00:05 +0200

    idk

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-// App.js
 import React from "react";
 import Pages from "./pages/Pages";
 import { BrowserRouter as Router } from "react-router-dom";
 
 const App = () => {
   return (
-    <Router>
+    <Router basename="/React-Website">
       <Pages />
     </Router>
   );
